c332b583d4fdd16ecb72db3b71e0e1d9f134c9b7
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-25 18:25:54 +0000

    Update elasticsearch.go

--- cmd/elasticsearch/elasticsearch.go
+++ cmd/elasticsearch/elasticsearch.go
@@ -4,33 +4,42 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package elasticsearch
 
 import (
+	"fmt"
+	"os/exec"
+
 	"github.com/spf13/cobra"
 )
 
+func List() {
+	cmd := exec.Command("ls", "./")
+
+	// The `Output` method executes the command and
+	// collects the output, returning its value
+	out, err := cmd.Output()
+	if err != nil {
+		// if there was any error, print it here
+		fmt.Println("could not run command: ", err)
+	}
+	// otherwise, print the output from running the command
+	fmt.Println("Output: ", string(out))
+
+}
+
+var err error
+
 // elasticsearchCmd represents the elasticsearch command
-func NewCmdCreate() *cobra.Command {
-	var ElasticsearchCmd = &cobra.Command{
+func NewCmdElastic() *cobra.Command {
+	var elasticsearchCmd *cobra.Command = &cobra.Command{
 		Use:   "elasticsearch",
 		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-		Run: func(cmd *cobra.Command, args []string) {
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if err != nil {
+				return err
+			}
+			return nil
 		},
 	}
-	return ElasticsearchCmd
-}
-func init() {
-	// Here you will define your flags and configuration settings.
+	elasticsearchCmd.AddCommand(enableCmdIndex())
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// elasticsearchCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// elasticsearchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	return elasticsearchCmd
 }
