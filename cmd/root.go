2d85fe6dfb5c0600196dc7477a1f7ef1d6c232e5
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-25 18:26:09 +0000

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ package cmd
 import (
 	"os"
 
-	elasticCmd "github.com/gitmapd/novo/cmd/elasticsearch"
+	"github.com/gitmapd/novo/cmd/elasticsearch"
 	"github.com/spf13/cobra"
 )
 
@@ -44,5 +44,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.AddCommand(elasticCmd.NewCmdCreate())
+	rootCmd.AddCommand(elasticsearch.NewCmdElastic())
 }
