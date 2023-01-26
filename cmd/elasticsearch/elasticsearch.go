040f9a8a1bb005e4e9b3e39a04249014abded4ba
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-26 20:30:15 +0000

    Update elasticsearch.go

--- cmd/elasticsearch/elasticsearch.go
+++ cmd/elasticsearch/elasticsearch.go
@@ -5,41 +5,25 @@ package elasticsearch
 
 import (
 	"fmt"
-	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
-func List() {
-	cmd := exec.Command("ls", "./")
-
-	// The `Output` method executes the command and
-	// collects the output, returning its value
-	out, err := cmd.Output()
-	if err != nil {
-		// if there was any error, print it here
-		fmt.Println("could not run command: ", err)
-	}
-	// otherwise, print the output from running the command
-	fmt.Println("Output: ", string(out))
-
-}
-
-var err error
-
 // elasticsearchCmd represents the elasticsearch command
-func NewCmdElastic() *cobra.Command {
-	var elasticsearchCmd *cobra.Command = &cobra.Command{
+func NewCmdCreate() *cobra.Command {
+	var elasticsearchCmd = &cobra.Command{
 		Use:   "elasticsearch",
 		Short: "A brief description of your command",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if err != nil {
-				return err
-			}
-			return nil
+		Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("elasticsearch called")
 		},
 	}
-	elasticsearchCmd.AddCommand(enableCmdIndex())
-
+	elasticsearchCmd.AddCommand(indexCmdCreate())
 	return elasticsearchCmd
 }
