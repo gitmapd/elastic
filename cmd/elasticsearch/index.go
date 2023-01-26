8d6ff79dce4849d876369974e524821ce6f1101b
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-26 20:30:47 +0000

    Update index.go

--- cmd/elasticsearch/index.go
+++ cmd/elasticsearch/index.go
@@ -4,32 +4,36 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package elasticsearch
 
 import (
+	"fmt"
+	"log"
+	"os/exec"
+
 	"github.com/spf13/cobra"
 )
 
+func execs() {
+	cmd := exec.Command("ls", "-lr")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	fmt.Printf("combined out:\n%s\n", string(out))
+}
+
 // indexCmd represents the index command
-/*var indexCmd = &cobra.Command{
-	Use:   "index",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+func indexCmdCreate() *cobra.Command {
+	var indexCmd = &cobra.Command{
+		Use:   "index",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(os.Args[1:])
-	},
-}
-*/
-
-func enableCmdIndex() *cobra.Command {
-	var indexCmd = &cobra.Command{
-		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			List()
+			execs()
 		},
 	}
 	return indexCmd
-
 }
