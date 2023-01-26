bd9b78e1a23cbddb356d3c7bf16ca81c13626e05
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-26 20:31:12 +0000

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -6,12 +6,12 @@ package cmd
 import (
 	"os"
 
-	"github.com/gitmapd/elastic/cmd/elasticsearch"
+	"github.com/gitmapd/novo/cmd/elasticsearch"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
+var RootCmd = &cobra.Command{
 	Use:   "novo",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
@@ -28,7 +28,7 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -43,6 +43,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.AddCommand(elasticsearch.NewCmdElastic())
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.AddCommand(elasticsearch.NewCmdCreate())
 }
