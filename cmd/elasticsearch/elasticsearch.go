137b96716f73959fc71f2eaf7940633524807f31
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-27 18:33:38 +0000

    Update elasticsearch.go

--- cmd/elasticsearch/elasticsearch.go
+++ cmd/elasticsearch/elasticsearch.go
@@ -21,7 +21,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("elasticsearch called")
+			fmt.Println("Error: must also specify subcommand like index")
 		},
 	}
 	elasticsearchCmd.AddCommand(indexCmdCreate())
